4032ad1312ac0c9c1344d541752aca3390b4207e
kleban <yurakleban@gmail.com>
2024-05-02 10:16:44 +0300

    Paging Added

--- KN-2 2024/AppDemo/Controllers/HomeController.cs
+++ KN-2 2024/AppDemo/Controllers/HomeController.cs
@@ -1,21 +1,54 @@
 using AppDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using X.PagedList;
 
 namespace AppDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private const int pageSize = 3;
         private readonly ILogger<HomeController> _logger;
+
+        List<Demo> data = new List<Demo>();
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _logger = logger;            
+            for (int i = 1; i < 30; i++)
+                data.Add(new Demo { Id = i, Name = $"Name_{i}" });
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public class Demo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public async Task<IActionResult> Paging(int? page)
+        {
+            // return a 404 if user browses to before the first page
+            page = page ?? 1;
+
+            if (page < 1)
+            {
+                return NotFound();
+            }
+                        
+            var records = await data.ToPagedListAsync((int)page, pageSize);
+
+            // return a 404 if user browses to pages beyond last page. special case first page if no items exist
+            if (records.PageNumber != 1 && page > records.PageCount)
+            {
+                return NotFound();
+            }
+
+            return View(records);
         }
 
         public IActionResult Map()
