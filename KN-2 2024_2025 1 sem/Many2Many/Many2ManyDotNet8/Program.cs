b37b02dbe3ded7593bc967f65c28007749831e9a
Yurii Kleban <yurakleban@gmail.com>
2024-11-18 11:58:20 +0200

    Update Program.cs

--- KN-2 2024_2025 1 sem/Many2Many/Many2ManyDotNet8/Program.cs
+++ KN-2 2024_2025 1 sem/Many2Many/Many2ManyDotNet8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Many2ManyDotNet8.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 var ctx = new UniContext();
 
@@ -12,3 +13,6 @@ foreach(var t in ctx.Teachers.Include(x=> x.Courses))
     }
     Console.WriteLine();
 }
+
+var date = DateTime.ParseExact("23..Jan..24 01:20", "dd..MMM..yy HH:mm", new CultureInfo("en"));
+Console.WriteLine(date);
