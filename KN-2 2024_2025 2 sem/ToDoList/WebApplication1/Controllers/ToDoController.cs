5daaa39ad4a6b23bb69644c30bbf766bcda15de2
Yurii Kleban <yurakleban@gmail.com>
2025-02-19 13:48:43 +0200

    fix

---
+++ KN-2 2024_2025 2 sem/ToDoList/WebApplication1/Controllers/ToDoController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var todos = _context.TodoItems.ToList();
+            return View(todos);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TodoItem todo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.TodoItems.Add(todo);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todo);
+        }
+
+        [HttpPost]
+        public IActionResult ToggleComplete(int id)
+        {
+            var todo = _context.TodoItems.Find(id);
+            if (todo != null)
+            {
+                todo.IsCompleted = !todo.IsCompleted;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var todo = _context.TodoItems.Find(id);
+            if (todo != null)
+            {
+                _context.TodoItems.Remove(todo);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ KN-2 2024_2025 2 sem/ToDoList/WebApplication1/Data/ToDoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
