c3ad7b98e8db88462395e12bdf3cc88193cd4fc5
Yurii Kleban <yurakleban@gmail.com>
2025-03-13 13:33:54 +0200

    OOP demo project

---
+++ Ek2 2025/OOPIntro/HRDemo/Models/SalesPerson.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRDemo.Models
+{
+    public class SalesPerson : Worker
+    {
+        protected double salesAmount;
+        public SalesPerson(string name, DateTime dob, double salary, double bonus, double sales = 0)
+            : base(name, dob, salary, bonus)
+        {
+            salesAmount = sales;
+        }
+
+        public double SalesAmount
+        {
+            get { return salesAmount; }
+            set
+            {
+                salesAmount = value;
+            }
+        }
+
+        public override string GetInfo()
+        {
+            string info = $"****** Sales Person *******\n" +
+               $"Name:\t{name}\nSalary:\t${GetTotalSalary()}\nAge:\t{Age} y.o.\nSales:\t${salesAmount}\n";
+            return info;
+        }
+
+        public override double GetTotalSalary()
+        {
+            return salary + bonus + salesAmount * 0.1;
+        }
+
+        public override string ToString()
+        {
+            return $"Sales Person: {name}, ${GetTotalSalary()}, {Age} y.o., ----> ${salesAmount} sales amount";
+        }
+    }
+}
---
+++ Ek2 2025/OOPIntro/HRDemo/Models/Worker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRDemo.Models
+{
+    public abstract class Worker
+    {
+        protected string name;
+        protected DateTime dob;
+        protected double salary;
+        protected double bonus;
+
+        public Worker(string name, DateTime dob, double salary, double bonus)
+        {
+            this.name = name;
+            this.dob = dob;
+            this.salary = salary;
+            this.bonus = bonus;
+        }
+
+        public int Age
+        {
+            get { return (int)(DateTime.Now - dob).TotalDays/365; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (salary == value)
+                    salary = value;
+            }
+        }
+
+        public double GetSalary()
+        {
+            return salary;
+        }
+
+        public void SetSalary(double salary)
+        {
+            if (salary >= 0)
+                this.salary = salary;
+        }
+
+        public virtual double GetTotalSalary()
+        {
+            return salary + bonus;
+        }
+
+        public abstract string GetInfo();
+
+        public override string ToString()
+        {
+            return $"Worker: {name}, ${GetTotalSalary()}, {Age} y.o.";
+        }
+    }
+}
