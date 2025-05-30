fe1429c88f53aefc60c31b533ba4c267893025f2
Yurii Kleban <yurakleban@gmail.com>
2025-05-30 11:58:31 +0300

    Zoo Added

---
+++ EK-2 2025/Zoo/Zoo/Models/CantAddAnimalException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Models
+{
+    public class CantAddAnimalException : ApplicationException
+    {
+        // Default constructor
+        public CantAddAnimalException(string msg) : base(msg)
+        {
+        }
+    }
+}
