602921284cd77d9ca77e8997172b5b5a9be34cca
Yurii Kleban <yurakleban@gmail.com>
2026-03-17 08:54:36 +0200

    fix

---
+++ 2026/EK2_2026/OOP1/OOP1/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    public class Product
+    {
+        //fields
+        private string name;
+        private double price;
+        private double quantity;
+
+        //ctor
+        public Product(string name, double price, double quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        //methods
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{name} - ${price} x {quantity}");
+        }
+
+        //props
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public double Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+    }
+}
---
+++ 2026/EK2_2026/OOP1/OOP1/Program.cs
@@ -0,0 +1,10 @@
+﻿using OOP1;
+
+Product[] products = new Product[2];
+products[0] = new Product("Milk", 0.95, 10);
+products[1] = new Product("Car", 12500, 3);
+
+foreach (var product in products)
+{
+    product.ShowInfo();
+}
